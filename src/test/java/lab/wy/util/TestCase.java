e156f0d71408940a0d10ee66b48ce684ea0895af
sweetpark <wooyeong1998@gmail.com>
2026-03-19 23:28:24 +0900

    feat : 백준 유틸 클래스 생성 - 메모리 체크 - 시간 체크 - 입력값 + 결과값 매칭

---
+++ src/test/java/lab/wy/util/TestCase.java
@@ -0,0 +1,15 @@
+package lab.wy.util;
+
+public class TestCase {
+    String input;
+    String expected;
+    long timeLimitMs;
+    long memoryLimitMb;
+
+    public TestCase(String input, String expected, long timeLimitMs, long memoryLimitMb) {
+        this.input = input;
+        this.expected = expected;
+        this.timeLimitMs = timeLimitMs;
+        this.memoryLimitMb = memoryLimitMb;
+    }
+}
